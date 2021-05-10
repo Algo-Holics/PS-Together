d3060af2c0872c4eada6f0eca9407c80933a7ad2
Jamie Lee <itzjamie96@gmail.com>
2021-05-11 00:45:41 +0900

    [Jamie] SORT 가장 큰 수

--- PROGRAMMERS/Sort/Jamie/L2_가장 큰 수.py
+++ PROGRAMMERS/Sort/Jamie/L2_가장 큰 수.py
@@ -1,25 +1,16 @@
+def solution(numbers):
+    # 숫자들의 비교를 위해 str로 변환
+    strNumbers = list(map(str, numbers))
 
+    # numbers의 원소는 1,000이하 = 3자리까지 가능
+    # 'n'*3을 한 후 3자리에서 자른 것을 기준으로 정렬
+    # ex) 2 => '222' / 31 => '313'
+    strNumbers.sort(key=lambda x: [x * 3][:3], reverse=True)
 
-# 시간초과
-def solution_(numbers):
-
-    tmp = map(str, numbers)
-    # 모든 숫자를 붙이면 나오는 길이 확인
-    length = len(''.join(tmp))
-
-    big_numbers = []
-    for i in range(len(numbers)):
-        num = str(numbers[i])
-        tmp = num * (length//len(num)) + num[:length%len(num)]
-        big_numbers.append((tmp, i))
-
-    big_numbers = sorted(big_numbers, reverse=True)
-    answer = ''
-    for number in big_numbers:
-        num, idx = number
-        answer += str(numbers[idx])
-    # print(answer)
-    return answer
+    # '001' 같은 경우는 int로 1이기 때문에 int()를 한 후 str()을 다시 해준다
+    # => 문자열로 바꿔서 리턴하는 게 조건
+    answer = ''.join(strNumbers)
+    return str(int(answer))
 
 
 solution([6, 10, 2])
